f85c7ccc69aa95d8ee2878826eac40374b24bd84
aizaz-dev <azazkhan2682000@gmail.com>
2026-03-21 21:07:31 +0500

    added spaces

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { ServiceHero } from '@/components/sections/services/ServiceHero'
 import { ServiceSection } from '@/components/sections/services/ServiceSection'
 import { CaseStudySection } from '@/components/sections/services/CaseStudySection'
